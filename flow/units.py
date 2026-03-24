a782e670e7035fc88bb8fa4829f7e3a1edc5be40
Dewangga <dewangga@gmail.com>
2026-03-24 16:29:26 +0700

    update code after lebaran`s day

--- flow/units.py
+++ flow/units.py
@@ -3,54 +3,58 @@ import random
 #karakter
 
 class Dasar_Karakter:
-    def __init__(self, hp, nama):
-        self.hp = hp
+    def __init__(self, nama, hp, atk):
         self.nama = nama
+        self.hp = hp
+        self.atk = atk
+        self.max_hp = hp
 
-    def status(self):
-        return f'status pertempuran kita sekarang\nnama: {self.nama}\nHP: {self.hp}'
-    
-class Elsa(Dasar_Karakter):
-    def __init__(self, hp, nama):
-        super().__init__(hp, nama)
-        self.atk = 5
-        self.ability = 3
-        self.hp = 75
+    def menyerang(self, target):
+        damage = self.atk
+        return target.menerima_serangan(damage)
 
-    def pasif_heal(self, daftar_tim):
-        for anggota in daftar_tim:
-            anggota.hp += self.ability
+    def menerima_serangan(self, damage):
+        self.hp -= damage
+        pesan = f'{self.nama} terkena damage, hp berkurang sebanyak {damage} \ndarah {self.nama} tersisa: {self.hp}'
+
+        if self.hp <= 0:
+            kalah = f'{self.nama} menyerah, pertarungan berakhir'
+            return kalah, False
+
+        return pesan, True
         
-        print(f'{self.nama} mengobati hp tim sebanyak {self.ability}')
 
-    def serang(self, lawan):
-        lawan.hp -= self.atk
+class Elsa(Dasar_Karakter):
+    def __init__(self, nama, hp, atk):
+        super().__init__ (nama, hp, atk)
+        self.abillity = 7
 
-        print(f'{self.nama} mencoba menyerang...!, dan berhasil mengurangi hp {lawan} sebanyak {self.atk}')
+    def pasif_healer(self, daftar_tim):
+        for anggota in daftar_tim.values():
+            if anggota.hp > 0:
+                anggota.hp += self.abillity
+                if anggota.hp > anggota.maks_hp:
+                    anggota.hp = anggota.maks_hp
+        print(f'{self.nama} mengobati hp tim sebanyak {self.abillity} \nhp setiap tim menjadi {daftar_tim.max(100)}')
 
 class Bruno(Dasar_Karakter):
-    def __init__(self, hp, nama):
-        super().__init__(hp, nama, tangkis = False)
-        self.atk = 12
-        self.hp = 150
+    def __init__(self, nama, hp, atk):
+        super().__init__(nama, hp, atk)
+        self.tangkis = False
 
-    def tangkis():
-        print(f'bruno menggunakan barbelnya, menangkis serangan monster dengan mudah...!')
-        if self.hp -= Monster.serang(Bruno):
-            Monster.serang(bruno) = False
+    def menangkis_serangan(self, damage):
+        if self.tangkis:
+            self.tangkis = False
+            pesan = f"{self.nama} menggunakan barbel super nya..!!! Tidak ada serangan monster yang terasa."
+            return pesan, True
 
-    def serang(self, lawan):
-        lawan.hp -= self.atk
-
-        print(f'{self.nama} mencoba menyerang...!, dan berhasil mengurangi hp {lawan} sebanyak {self.atk}')
-
-
+        return super().menerima_serangan(damage)
 
 #monster
 
 class Monster(Dasar_Karakter):
-    def __init__(self, hp, nama):
-        super().__init__(hp, nama)
+    def __init__(self, nama, hp, atk):
+        super().__init__(hp, nama, atk)
         self.daftar_atk = [40,15,17,13]
        
     def serang(self, target):
