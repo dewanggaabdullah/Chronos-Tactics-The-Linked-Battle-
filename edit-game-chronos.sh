ee61281ceb0651ca19b2e64939d9e4700f5b8612
dewangga <255674162+dewanggaabdullah@users.noreply.github.com>
2026-05-31 07:03:54 +0700

    refresh branch
    Signed-off-by: dewangga <255674162+dewanggaabdullah@users.noreply.github.com>

--- edit-game-chronos.sh
+++ edit-game-chronos.sh
@@ -1,7 +1,6 @@
 echo "⚙️  Memulai proses Build Ulang (Compile) Kontainer Chronos-Tactics... ⚔️"
 echo "----------------------------------------------------------------------"
 sudo docker build -t chronos-tactics:latest .
-
 echo "----------------------------------------------------------------------"
 echo "✅ Build Selesai! Kontainer kamu sudah diperbarui dengan kode game terbaru."
 echo "💡 Untuk memainkannya, silakan jalankan: ./jalankan-game-cronos.sh"
